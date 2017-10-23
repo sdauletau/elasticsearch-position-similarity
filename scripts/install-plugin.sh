bba1d9ad8b27d1523a027073d5507c6ff88a1b61
sdauletau <sdauletau@gmail.com>
2017-10-23 12:09:23 -0500

    elasticsearch 6.0.0-rc1

--- scripts/install-plugin.sh
+++ scripts/install-plugin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/opt/elasticsearch-6.0.0-beta2/bin/elasticsearch-plugin install file:///`pwd`/build/distributions/elasticsearch-position-similarity-6.0.0-beta2.zip
+/usr/local/opt/elasticsearch-6.0.0-rc1/bin/elasticsearch-plugin install file:///`pwd`/build/distributions/elasticsearch-position-similarity-6.0.0-rc1.zip
