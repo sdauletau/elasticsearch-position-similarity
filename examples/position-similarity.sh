01b4e8d3c9fe96500e67420fabf503635e9a8937
sdauletau <sdauletau@gmail.com>
2019-04-21 14:10:30 -0500

    elasticsearch 7.0.0

--- examples/position-similarity.sh
+++ examples/position-similarity.sh
@@ -40,7 +40,7 @@ curl --header "Content-Type:application/json" -s -XPUT "http://localhost:9200/te
 echo
 
 echo 'add mappings'
-curl --header "Content-Type:application/json" -XPUT 'localhost:9200/test_index/_mapping/test_type' -d '
+curl --header "Content-Type:application/json" -XPUT 'localhost:9200/test_index/_mapping' -d '
 {
   "properties": {
     "field1": {
@@ -57,25 +57,25 @@ curl --header "Content-Type:application/json" -XPUT 'localhost:9200/test_index/_
 echo
 
 echo 'index doc 1'
-curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/test_type/1" -d '
+curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/_doc/1" -d '
 {"field1" : "bar foo", "field2" : "bar|0 foo|1"}
 '
 echo
 
 echo 'index doc 2'
-curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/test_type/2" -d '
+curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/_doc/2" -d '
 {"field1" : "foo foo bar bar bar", "field2" : "foo|0 foo|1 bar|3 bar|4 bar|5"}
 '
 echo
 
 echo 'index doc 3'
-curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/test_type/3" -d '
+curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/_doc/3" -d '
 {"field1" : "bar bar foo too", "field2" : "bar|0 bar|1 foo|2 too|3"}
 '
 echo
 
 echo 'index doc 4'
-curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/test_type/4" -d '
+curl --header "Content-Type:application/json" -s -XPUT "localhost:9200/test_index/_doc/4" -d '
 {"field1" : "bar bar too", "field2" : "bar|0 bar|1 too|2"}
 '
 echo
@@ -86,7 +86,7 @@ echo
 
 echo
 echo 'expecting doc 2 to have highest score'
-curl --header "Content-Type:application/json" -s "localhost:9200/test_index/test_type/_search?pretty=true" -d '
+curl --header "Content-Type:application/json" -s "localhost:9200/test_index/_search?pretty=true" -d '
 {
   "explain": false,
   "query": {
@@ -104,7 +104,7 @@ echo
 
 echo
 echo 'expecting doc 4 to have highest score'
-curl --header "Content-Type:application/json" -s "localhost:9200/test_index/test_type/_search?pretty=true" -d '
+curl --header "Content-Type:application/json" -s "localhost:9200/test_index/_search?pretty=true" -d '
 {
   "explain": true,
   "from": 0,
